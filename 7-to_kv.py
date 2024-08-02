b5c0d608de6f2c149ccb3162dd3fcb28621b64f5
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2024-08-02 23:41:44 +0530

    all task

---
+++ 7-to_kv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+type-annotated function to_kv that takes a string k and an int OR float v
+as arguments and returns a tuple.
+The first element of the tuple is the string k
+The second element is the square of the int/float v
+and should be annotated as a float.
+'''
+
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''
+    takes str and int or float
+    returns tuple with str and float
+    '''
+    return (k, v ** 2)
---
+++ 8-make_multiplier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+type-annotated function make_multiplier that takes a float multiplier
+as argument and returns a function that multiplies a float by multiplier.
+'''
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''
+    takes a float
+    returns a function that takes a float and returns a float
+    '''
+    def mult(n: float) -> float:
+        '''
+        multiplies float n by multiplier from above
+        '''
+        return n * multiplier
+    return mult
